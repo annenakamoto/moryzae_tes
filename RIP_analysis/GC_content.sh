a1009aee820e7940969f20e5521eb5a946f41fe9
annenakamoto <67307294+annenakamoto@users.noreply.github.com>
2022-11-25 23:06:11 -0800

    Update GC_content.sh

--- RIP_analysis/GC_content.sh
+++ RIP_analysis/GC_content.sh
@@ -12,24 +12,24 @@
 
 cd  /global/scratch/users/annen
 
-# ### GC content for TEs
-# while read TE; do
-#     geecee -sequence /global/scratch/users/annen/Rep_TE_Lib/Align_TEs/REPHITS_${TE}.fasta -outfile RIP_analysis/gc_${TE}.txt
-#     avg=$(cat RIP_analysis/gc_${TE}.txt | awk 'BEGIN { n=0; s=0; } { n+=1; s+=$2; } END { print s/n }')
-#     echo "$TE: $avg"
-# done < TEs_list.txt
+### GC content for TEs
+while read TE; do
+    geecee -sequence /global/scratch/users/annen/Rep_TE_Lib/Align_TEs/REPHITS_${TE}.fasta -outfile RIP_analysis/gc_${TE}.txt
+    avg=$(cat RIP_analysis/gc_${TE}.txt | awk 'BEGIN { n=0; s=0; } { n+=1; s+=$2; } END { print s/n }')
+    echo "$TE: $avg"
+done < TEs_list.txt
 
-# ### GC content for genomes
-# while read GENOME; do
-#     geecee -sequence /global/scratch/users/annen/JC_gist_genomes/${GENOME}.cds.fasta -outfile RIP_analysis/gc_${GENOME}.txt
-#     avg=$(cat RIP_analysis/gc_${GENOME}.txt | awk 'BEGIN { n=0; s=0; } { n+=1; s+=$2; } END { print s/n }')
-#     echo "$GENOME: $avg"
-# done < rep_genomes_list.txt
+### GC content for genomes
+while read GENOME; do
+    geecee -sequence /global/scratch/users/annen/JC_gist_genomes/${GENOME}.cds.fasta -outfile RIP_analysis/gc_${GENOME}.txt
+    avg=$(cat RIP_analysis/gc_${GENOME}.txt | awk 'BEGIN { n=0; s=0; } { n+=1; s+=$2; } END { print s/n }')
+    echo "$GENOME: $avg"
+done < rep_genomes_list.txt
 
-# > GC_data.txt
-# while read TE; do
-#     cat RIP_analysis/gc_${TE}.txt | awk -v TE=$TE '{ print TE "\t" $1 "\t" $2 }' >> GC_data.txt
-# done < TEs_list.txt
+> GC_data.txt
+while read TE; do
+    cat RIP_analysis/gc_${TE}.txt | awk -v TE=$TE '{ print TE "\t" $1 "\t" $2 }' >> GC_data.txt
+done < TEs_list.txt
 
 
 ### creating TE libraries using just guy11 and FJ98099
